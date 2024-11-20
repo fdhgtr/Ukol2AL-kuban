49bc3a50d286efbe37f8985413e1a3ef0458cbcf
Tobias <159123644+fdhgtr@users.noreply.github.com>
2024-11-20 09:30:06 +0000

    gf

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 array = [13,56,6,85,3,45,49,78,1,65]
+
+def bubble_sort():
+    n = len(array)
+    for i in range(n-1):
+        for j in range(0,n-i-1):
+            if array[j] > array[j+1]:
+                array[j], array[j+1] = array[j+1], array[j]
+                return array
+
+print(bubble_sort())
+
+
+            
