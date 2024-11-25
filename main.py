4bef42119d0c4927f5fd15c2239abfa577033c3e
Tobias <159123644+fdhgtr@users.noreply.github.com>
2024-11-25 11:19:26 +0000

    fd

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 array = [13,56,6,85,3,45,49,78,1,65]
 
 def bubble_sort():
@@ -9,6 +10,25 @@ def bubble_sort():
                 return array
 
 print(bubble_sort())
+arraya = [78,26,58,7,5,98,43,63,4,2]
+def is_sorted(arrayb):
+    for i in range(1, len(arrayb)):
+        if arrayb[i] < arrayb[i-1]:
+            return False
+    return True
+
+def bogosort(arrayb):
+    count = 0
+    while not is_sorted(arrayb):
+        random.shuffle(arrayb)
+        count += 1
+        print(f"Pokus {count}: {arrayb}")
+    print(f"Sorted array after {count} shuffles: ")
+    bogosort(arrayb)
+    print("Bogo",arrayb)
+    
+
+
 
 
             
